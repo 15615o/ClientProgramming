95f31a3c0290d2c6f39e502a9bfea49574ecb36b
15615o <kimsr042931@gmail.com>
2025-06-07 11:33:53 +0900

    week15

--- src/router/PostRouter.jsx
+++ src/router/PostRouter.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import ListPage from '../components/post/ListPage'
 import WritePage from '../components/post/WritePage'
+import ReadPage from '../components/post/ReadPage'
+import EditPage from '../components/post/EditPage'
 
 const PostRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<ListPage/>} />
             <Route path='/write' element={<WritePage/>} />
+            <Route path='/:id' element={<ReadPage/>} />
+            <Route path='/edit/:id' element={<EditPage/>} />
         </Routes>
     )
 }
